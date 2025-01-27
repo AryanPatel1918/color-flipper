5ca62e2f1d7de959a784b58456e5b40501532cdc
Aryan Patel <aryanpatel1918@gmail.com>
2025-01-27 16:09:35 -0500

    first commit

---
+++ Color Flipper/script.js
@@ -0,0 +1,22 @@
+function changeColour() {
+
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    let a = Math.random().toFixed(1);
+
+    let randomColor = `rgba(${red}, ${green}, ${blue}, ${a})`;
+    document.body.style.backgroundColor = randomColor;
+
+    let currentColour = document.getElementById('currentColour');
+    currentColour.style.opacity = 0; // opacity takes 1s to change
+
+    // After the opacity transition (1s), update the text and make it fade in
+    setTimeout(function() {
+        currentColour.innerHTML = randomColor; // Change the text content
+        currentColour.style.opacity = 1;  // Fade the text back in
+    }, 1000);  // Match the 1s transition time
+}
+
+
+
